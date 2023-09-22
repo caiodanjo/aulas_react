a835522c7d7646e9be154668dfd85ad55a1e8e83
caiodanjo <caio.danjo@gmail.com>
2023-09-22 15:18:47 -0300

    projeto finalizado

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -2,7 +2,13 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo voluptatibus esse voluptatum quis earum magni totam dolore alias ipsa fuga, necessitatibus vel quisquam quod? Quam, iusto. Aliquam, dolore provident. Ex.
+      Veniam, voluptatibus quisquam. Nemo rerum numquam quo esse similique fugit. Mollitia facere dolor optio placeat quam! Quam, consequuntur voluptas, harum laboriosam id debitis adipisci exercitationem repudiandae ratione eius laudantium impedit.
+      Ea ducimus quidem inventore officia illo culpa laborum odio, quaerat fugiat ad veritatis tempora autem, odit nam esse debitis quo at eaque tempore repellat laboriosam itaque. Sapiente suscipit ab voluptatibus?
+      Optio eaque sunt consectetur ducimus, corrupti rem! Commodi saepe corrupti harum corporis nemo iste. Quidem corporis aspernatur quisquam accusantium temporibus dolor, repellat recusandae accusamus sint placeat praesentium, impedit facere blanditiis.</p>
+    </div>
   )
 }
 
